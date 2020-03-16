80d5e5c73ac23e800c50f5438f1c372e082a59c8
Oleksandr Dehtiar <oleksandr.dehtiar@nure.ua>
2020-03-16 02:31:12 +0200

    Added spdlog library

--- sample/adc_sample.cpp
+++ sample/adc_sample.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <thread>
 #include "open_greenery/adc/ADCFactory.hpp"
+#include "spdlog/spdlog.h"
 
 namespace oga = open_greenery::adc;
 namespace ogd = open_greenery::driver;
@@ -10,21 +11,38 @@ using clk = chr::system_clock;
 
 int main()
 {
+    using ADC_MUX = ogd::ADS1115::MUX;
     // ADC reading sample
+    spdlog::set_pattern("%v");
+    constexpr ADC_MUX adc_ports [4] = {ADC_MUX::SINGLE_0,
+                                      ADC_MUX::SINGLE_1,
+                                      ADC_MUX::SINGLE_2,
+                                      ADC_MUX::SINGLE_3};
+
+    oga::ADCFactory & adc_factory = oga::ADCFactory::getInstance();
+    std::map<ADC_MUX, std::shared_ptr<oga::IADCReader>> adc_readers;
+    for (const auto port : adc_ports)
+    {
+        adc_readers[port] = adc_factory.getReader(ogd::ADS1115::Address::GND, port);
+    }
+
+    clk::time_point end = clk::now() + chr::seconds(30);
     try
     {
-        auto a0 = oga::ADCFactory::getInstance().getReader(ogd::ADS1115::Address::GND, ogd::ADS1115::MUX::SINGLE_0);
-
-        clk::time_point end = clk::now() + chr::seconds(10);
         while (clk::now() < end)
         {
-            std::cout << "A0: " << a0->read() << std::endl;
-            std::this_thread::sleep_for(chr::milliseconds(100));
+            spdlog::info("A0: {:5d} | A1: {:5d} | A2: {:5d} | A3: {:5d}",
+                         adc_readers[ADC_MUX::SINGLE_0]->read(),
+                         adc_readers[ADC_MUX::SINGLE_1]->read(),
+                         adc_readers[ADC_MUX::SINGLE_2]->read(),
+                         adc_readers[ADC_MUX::SINGLE_3]->read());
+
+            std::this_thread::sleep_for(chr::milliseconds(500));
         }
     }
     catch(const std::exception & e)
     {
-        std::cerr << e.what() << std::endl;
+        spdlog::error(e.what());
     }
 
     return 0;
