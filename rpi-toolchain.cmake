267362e89e7802670e6a0157427eb79ab409fe88
Oleksandr Dehtiar <oleksandr.dehtiar@nure.ua>
2020-05-06 01:44:12 +0300

    Toolchain updated for C++17
    Compilers select by environment variables
    Removed excess references to the toolchain
    Removed legacy Raspberry Pi toolchain submodule

--- rpi-toolchain.cmake
+++ rpi-toolchain.cmake
@@ -1,15 +1,22 @@
-# From https://medium.com/@au42/the-useful-raspberrypi-cross-compile-guide-ea56054de187
 # Define our host system
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_VERSION 1)
+set( CMAKE_SYSTEM_NAME    Linux )
+set( CMAKE_SYSTEM_VERSION    1 )
+
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER   ${CMAKE_CURRENT_SOURCE_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER ${CMAKE_CURRENT_SOURCE_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
-# Define the sysroot path for the RaspberryPi distribution in our tools folder 
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_CURRENT_SOURCE_DIR}/tools/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot/)
-# Use our definitions for compiler tools
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# Search for libraries and headers in the target directories only
-SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra")
+if( DEFINED ENV{OPEN_GREENERY_C_COMPILER} )
+    set( CMAKE_C_COMPILER    $ENV{OPEN_GREENERY_C_COMPILER} )
+else()
+    message( FATAL_ERROR    "Undefined OPEN_GREENERY_C_COMPILER environment variable. Cross compiler must be specified." )
+endif()
+
+if( DEFINED ENV{OPEN_GREENERY_CXX_COMPILER} )
+    set( CMAKE_CXX_COMPILER    $ENV{OPEN_GREENERY_CXX_COMPILER} )
+else()
+    message( FATAL_ERROR    "Undefined OPEN_GREENERY_CXX_COMPILER environment variable. Cross compiler must be specified." )
+endif()
+
+# Path to cross compilation Qt configurations
+set( CMAKE_PREFIX_PATH    /usr/lib/arm-linux-gnueabihf/cmake )
+
+set( CMAKE_CXX_FLAGS    "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra" )
+set( CMAKE_CXX_STANDARD_REQUIRED    ON )
