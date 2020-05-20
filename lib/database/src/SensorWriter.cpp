475a0ef6ffcb427a3b5bf9fce1cb6587d9afdd75
Oleksandr Dehtiar <oleksandr.dehtiar@nure.ua>
2020-05-21 01:50:04 +0300

    Database Writer and Reader
    SQLite wrappers for saving and getting analog sensor data
    Database usage sample

---
+++ lib/database/src/SensorWriter.cpp
@@ -0,0 +1,54 @@
+#include "open_greenery/database/SensorWriter.hpp"
+#include <exception>
+#include <functional>
+
+namespace open_greenery
+{
+namespace database
+{
+
+SensorWriter::SensorWriter(const Table _table)
+    :m_table(_table)
+{
+    m_table.database->exec("CREATE TABLE IF NOT EXISTS "+ _table.name +
+        " (time DATETIME PRIMARY KEY NOT NULL, value INTEGER NOT NULL)");
+}
+
+void SensorWriter::write(const std::int16_t _data)
+{
+    write("now", _data);
+}
+
+void SensorWriter::write(const QDateTime _timestamp, const std::int16_t _data)
+{
+    const auto time = _timestamp.toTimeSpec(Qt::UTC)
+                               .toString("yyyy-MM-dd hh:mm:ss")
+                               .toStdString()
+                               .c_str();
+
+    write(time, _data);
+}
+
+void SensorWriter::write(const char * _time, const std::int16_t _data)
+{
+    const auto data_str = std::to_string(_data);
+    SQLite::Transaction transaction(*m_table.database);
+
+    int rv = m_table.database->exec("INSERT INTO " + m_table.name + " VALUES "+
+            "((SELECT strftime('%Y-%m-%d %H:%M:%f', '" + _time + "')), " + data_str + ")");
+
+    if (rv != 1)
+    {
+        throw std::runtime_error("Database Error: Insert " + data_str +
+                                 " to the " + m_table.name + " returned " + std::to_string(rv));
+    }
+    transaction.commit();
+}
+
+Table SensorWriter::table() const
+{
+    return m_table;
+}
+
+}
+}
---
+++ sample/db_sample.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <QDebug>
+#include "open_greenery/adc/ADCFactory.hpp"
+#include "open_greenery/database/SensorReader.hpp"
+#include "open_greenery/database/SensorWriter.hpp"
+#include "open_greenery/sensor/AnalogSensor.hpp"
+
+namespace og = open_greenery;
+
+int main ()
+{
+    // ADS1115 A0 port reader
+    auto adc_reader = og::adc::ADCFactory::getInstance().getReader(
+            og::driver::ADS1115::Address::GND,
+            og::driver::ADS1115::MUX::SINGLE_0);
+    constexpr auto period = std::chrono::milliseconds(500);
+
+    // Publisher of ADC readers data each 0.5 sec
+    og::sensor::AnalogSensor a0(adc_reader, period);
+
+    // Open database file
+    auto db = std::make_shared<SQLite::Database>("/tmp/sensor_data.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+    std::cout << "SQLite database file " << db->getFilename() << " opened successfully" << std::endl;
+
+    const og::database::Table table(db, "A0");// Declare the database table for sensor data
+    og::database::SensorWriter writer(table);// Database sensor writer saves data to the table
+
+    // Notificator that saves sensor data to the database
+    og::sensor::AnalogSensor::Notificator ntr = [&writer](const std::int16_t _val)
+    {
+        try
+        {
+            writer.write(_val);
+            std::cout << "ADC value [" << _val << "] successfully saved" << std::endl;
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << "SQLite exception: " << e.what() << std::endl;
+        }
+    };
+    a0.subscribe(ntr);// Transfer notificator to the A0 sensor
+    // Pause main thread; Sensor publisher continues save ADC data via SensorWriter in the Notificator
+    const auto from = QDateTime::currentDateTime();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    const auto to = QDateTime::currentDateTime();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    a0.disable();
+
+    if (table.valid())
+    {
+        og::database::SensorReader reader(table);
+        std::vector<og::database::SensorData> res = reader.read(from, to);
+        for (const auto pair : res)
+        {
+            const auto [dt, val] = pair;
+            qDebug() << dt << " | " << val;
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid table: " << table.name << std::endl;
+    }
+
+    return 0;
+}
