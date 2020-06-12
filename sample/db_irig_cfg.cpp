a242b05838e56ff8aaa83ce9a83b121b3f4b9e1f
Oleksandr Dehtiar <oleksandr.dehtiar@nure.ua>
2020-06-12 23:48:05 +0300

    Updated irrigation configuration sample

--- sample/db_irig_cfg.cpp
+++ sample/db_irig_cfg.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <open_greenery/database/IrrigationConfigReader.hpp>
 #include <open_greenery/database/IrrigationConfigWriter.hpp>
-#include <open_greenery/gpio/GPIOFactory.hpp>
+#include <open_greenery/gpio/PinId.hpp>
 
 namespace og = open_greenery;
 
 int main()
 {
     std::cout << "Create/open DB" << std::endl;
-    auto db = std::make_shared<SQLite::Database>("/tmp/irig.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-    std::cout << ((db != nullptr) ? "not nullptr":"nullptr") << std::endl;
+    auto db = std::make_shared<SQLite::Database>("/home/pi/og/db/open_greenery.db3", SQLite::OPEN_READWRITE);
     std::cout << "SQLite database file " << db->getFilename() << " opened successfully" << std::endl;
     og::database::IrrigationConfigWriter writer ({db, "IrrigationConfig"});
 
-    open_greenery::gpio::PinId pin {2, og::gpio::Pinout::WIRING_PI};
-    std::int16_t dry {13000};
-    std::int16_t wet {5000};
+    open_greenery::gpio::PinId pin {0, og::gpio::Pinout::WIRING_PI};
+    std::int16_t dry {13500};
+    std::int16_t wet {8000};
     std::uint16_t watering_volume {10};
-    std::chrono::seconds watering_period {10};
-    open_greenery::database::SensorReader soil_moisture_reader ({db, "A0"});
+    std::chrono::seconds watering_period {5};
+    open_greenery::database::SensorReader soil_moisture_reader ({db, "A3"});
     writer.write({pin, dry, wet, watering_volume, watering_period, soil_moisture_reader});
 
     og::database::IrrigationConfigReader reader ({db, "IrrigationConfig"});
