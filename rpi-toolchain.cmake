6f31439fb0646ec2342387c47fe277b2411ed358
Oleksand Dehtiar <oleksandr.dehtiar@nure.ua>
2020-08-26 19:15:18 +0300

    Removed compilers selection environment variables
    Preferably use cmake command line arguments to specify compilers

--- rpi-toolchain.cmake
+++ rpi-toolchain.cmake
@@ -2,19 +2,6 @@
 set( CMAKE_SYSTEM_NAME    Linux )
 set( CMAKE_SYSTEM_VERSION    1 )
 
-# Define the cross compiler locations
-if( DEFINED ENV{OPEN_GREENERY_C_COMPILER} )
-    set( CMAKE_C_COMPILER    $ENV{OPEN_GREENERY_C_COMPILER} )
-else()
-    message( FATAL_ERROR    "Undefined OPEN_GREENERY_C_COMPILER environment variable. Cross compiler must be specified." )
-endif()
-
-if( DEFINED ENV{OPEN_GREENERY_CXX_COMPILER} )
-    set( CMAKE_CXX_COMPILER    $ENV{OPEN_GREENERY_CXX_COMPILER} )
-else()
-    message( FATAL_ERROR    "Undefined OPEN_GREENERY_CXX_COMPILER environment variable. Cross compiler must be specified." )
-endif()
-
 # Path to cross compilation Qt configurations
 set( CMAKE_PREFIX_PATH    /usr/lib/arm-linux-gnueabihf/cmake )
 
