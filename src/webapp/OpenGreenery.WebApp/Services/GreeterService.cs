ad4d6a14007f45e78c1e628b9e400d67b555a425
Andrew Rublyov <andrewrublyov99@gmail.com>
2021-07-10 01:44:15 +0300

    Create a bootstrap for a web app

---
+++ src/webapp/OpenGreenery.WebApp/Services/GreeterService.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using OpenGreenery.DeviceHub;
+
+namespace OpenGreenery.WebApp.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ src/webapp/OpenGreenery.WebApp/Shared/GreeneryThemes.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using MudBlazor;
+
+namespace OpenGreenery.WebApp.Shared
+{
+    public class NamedMudTheme : MudTheme
+    {
+        public string Name { get; init; }
+    }
+
+    public static class GreeneryThemes
+    {
+        public static NamedMudTheme Light = new NamedMudTheme
+        {
+            Name = "light",
+            Palette = new Palette
+            {
+                Primary = Colors.Green.Default,
+            },
+        };
+
+        public static NamedMudTheme Dark = new NamedMudTheme
+        {
+            Name = "dark",
+            Palette = new Palette
+            {
+                Primary = Colors.Green.Darken2,
+                Black = "#27272f",
+                Background = "#32333d",
+                BackgroundGrey = "#27272f",
+                Surface = "#373740",
+                DrawerBackground = "#27272f",
+                DrawerText = "rgba(255,255,255, 0.50)",
+                DrawerIcon = "rgba(255,255,255, 0.50)",
+                AppbarBackground = "#27272f",
+                AppbarText = "rgba(255,255,255, 0.70)",
+                TextPrimary = "rgba(255,255,255, 0.70)",
+                TextSecondary = "rgba(255,255,255, 0.50)",
+                ActionDefault = "#adadb1",
+                ActionDisabled = "rgba(255,255,255, 0.26)",
+                ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+                Divider = "rgba(255,255,255, 0.12)",
+                DividerLight = "rgba(255,255,255, 0.06)",
+                TableLines = "rgba(255,255,255, 0.12)",
+                LinesDefault = "rgba(255,255,255, 0.12)",
+                LinesInputs = "rgba(255,255,255, 0.3)",
+                TextDisabled = "rgba(255,255,255, 0.2)",
+            },
+        };
+
+        public static IReadOnlyDictionary<string, NamedMudTheme> Instances = new Dictionary<string, NamedMudTheme>
+        {
+            { Light.Name, Light },
+            { Dark.Name, Dark },
+        };
+    }
+}
