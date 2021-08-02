0470ff822e87a0bba70581d120cfb39bf0130236
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-08-03 00:05:08 +0300

    Device: Step-by-step creation of pump

--- src/device/sample/pump_sample.cpp
+++ src/device/sample/pump_sample.cpp
@@ -1,4 +1,6 @@
-#include "open_greenery/pump/Pump.hpp"
+#include <open_greenery/pump/Pump.hpp>
+#include <open_greenery/relay/Relay.hpp>
+#include <open_greenery/gpio/GPIOFactory.hpp>
 #include <iostream>
 
 namespace og = open_greenery;
@@ -6,7 +8,11 @@ namespace og = open_greenery;
 int main()
 {
     std::cout << "Create pump instance." << std::endl;
-    og::pump::Pump pump ({7u, og::gpio::Pinout::WIRING_PI});
+    auto pin = og::gpio::GPIOFactory::getInstance().getOutputGPIOctl(
+        {7u, og::gpio::Pinout::WIRING_PI}
+    );
+    auto relay = std::make_shared<og::relay::Relay>(pin);
+    og::pump::Pump pump (relay);
     std::cout << "Enter volume of watering in ml:" << std::endl;
     unsigned int volume {};
     std::cin >> volume;
