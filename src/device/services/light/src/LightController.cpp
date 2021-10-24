452758c6ae3a66639c59ab4dc233dad8bb8ecdb5
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-10-24 21:09:54 +0300

    Light service logging

--- src/device/services/light/src/LightController.cpp
+++ src/device/services/light/src/LightController.cpp
@@ -1,6 +1,8 @@
 #include "open_greenery/light/LightController.hpp"
+#include <open_greenery/dataflow/time/Participants.hpp>
 #include <chrono>
 #include <utility>
+#include <spdlog/spdlog.h>
 
 namespace ogl = open_greenery::dataflow::light;
 namespace ogt = open_greenery::dataflow::time;
@@ -48,6 +50,7 @@ open_greenery::tools::FinishFuture LightController::start()
                 THREAD_PERIOD
         );
     }
+    spdlog::info("Controller start");
     return m_service_thr->start();
 }
 
@@ -58,17 +61,20 @@ void LightController::stop()
         return;
     }
 
+    spdlog::info("Controller stop");
     m_service_thr->stop();
     m_service_thr.reset();
 }
 
 void LightController::LightServiceThreadFunc()
 {
-    // Update mode if control command received
+    // Update mode if control command
+    spdlog::trace("Request mode");
     auto new_mode = m_mode_provider->get();
     if (new_mode.has_value())
     {
         m_current_mode = new_mode.value();
+        spdlog::debug("Mode updated: {}", (m_current_mode == ogl::Mode::AUTO ? "AUTO" : "MANUAL"));
     }
 
     if (m_current_mode == ogl::Mode::AUTO)
@@ -81,27 +87,36 @@ void LightController::LightServiceThreadFunc()
 
 void LightController::handleAutomaticControl()
 {
+    spdlog::trace("Handle auto mode");
+
     auto is_event = [this](QTime some_time, QTime event_time) {
         return (some_time >= event_time) && (some_time < event_time.addMSecs(THREAD_PERIOD.count()));
     };
 
     // Update config if new one received
+    spdlog::trace("Request config");
     auto new_config = m_config_provider->get();
     if (new_config.has_value())
     {
         m_current_config = new_config.value();
+        spdlog::debug("Config updated");
     }
 
+    spdlog::trace("Current time request");
     const auto current_time = m_current_time_provider->get();
     if (is_event(current_time, m_current_config.day_start) && !m_relay->enabled())
     {
+        spdlog::info("Time to enable");
         m_relay->enable();
         // Inform receiver about status update
+        spdlog::trace("Send status");
         m_status_receiver->set(m_relay->enabled());
     }
     if (is_event(current_time, m_current_config.day_end) && m_relay->enabled())
     {
+        spdlog::info("Time to disable");
         m_relay->disable();
+        spdlog::trace("Send status");
         m_status_receiver->set(m_relay->enabled());
     }
 }
@@ -109,27 +124,33 @@ void LightController::handleAutomaticControl()
 void LightController::handleManualControl()
 {
     // Get control command if received
+    spdlog::trace("Manual control request");
     const auto command = m_manual_control_provider->get();
     if (!command.has_value())
     {
+        spdlog::trace("Manual control request");
         return;
     }
 
     // Handle command
     if (*command == open_greenery::dataflow::light::Control::DISABLE)
     {
+        spdlog::info("Disable relay");
         m_relay->disable();
     }
     else if (*command == open_greenery::dataflow::light::Control::ENABLE)
     {
+        spdlog::info("Enable relay");
         m_relay->enable();
     }
     else if (*command == open_greenery::dataflow::light::Control::TOGGLE)
     {
+        spdlog::info("Toggle relay");
         m_relay->toggle();
     }
 
     // Inform receiver about status
+    spdlog::trace("Send status");
     m_status_receiver->set(m_relay->enabled());
 }
 
