b09f0dd9e2b38e34daf4ac11c4284b6a6b8fd45b
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-10-24 21:40:17 +0300

    Warning logging level for tests executable

--- src/device/tests/main.cpp
+++ src/device/tests/main.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
 
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
+    spdlog::set_level(spdlog::level::warn);
     return RUN_ALL_TESTS();
 }
