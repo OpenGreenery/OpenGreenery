163256496a986632acb620ae0a059df1e742d830
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-11-05 16:59:46 +0200

    Relay RPC refactoring
    Light renamed with Relay to support ventilation in future
    Light service now is server and Proxy is client
    Periodic requests replaced with async data accessors

---
+++ src/device/lib/rpc/relay/src/Client.cpp
@@ -0,0 +1,120 @@
+#include "open_greenery/rpc/relay/Client.hpp"
+
+#include <utility>
+#include <grpcpp/create_channel.h>
+#include <grpcpp/channel.h>
+#include <spdlog/spdlog.h>
+
+namespace open_greenery::rpc::relay
+{
+
+Client::Client(const std::string & host)
+        :m_channel(grpc::CreateChannel(host, grpc::InsecureChannelCredentials())),
+        m_stub(Relay::NewStub(m_channel)) {}
+
+void Client::set(open_greenery::dataflow::light::LightConfigRecord record)
+{
+    grpc::ClientContext context;
+    Config request;
+    google::protobuf::Empty response;
+
+    const auto day_start = record.day_start.msecsSinceStartOfDay();
+    request.set_day_start(day_start);
+    const auto day_end = record.day_end.msecsSinceStartOfDay();
+    request.set_day_end(day_end);
+
+    auto TimeStr = [](const QTime & t){return t.toString("hh:mm:ss").toStdString();};
+    spdlog::debug("Send relay config: start={}, end={}",
+                  TimeStr(record.day_start),
+                  TimeStr(record.day_end));
+
+    auto status = m_stub->SetConfig(&context, request, &response);
+    if (!status.ok())
+    {
+        spdlog::warn("Unsuccessful Relay::SetConfig request: {} {}",
+                     status.error_code(),
+                     status.error_message());
+    }
+}
+
+void Client::set(open_greenery::dataflow::light::Control control)
+{
+    grpc::ClientContext context;
+    ManualControlRequest request;
+    google::protobuf::Empty response;
+
+    switch (control)
+    {
+        case open_greenery::dataflow::light::Control::ENABLE:
+            spdlog::debug("Send manual enable");
+            request.set_control(ManualControlRequest::CONTROL_ENABLE);
+            break;
+        case open_greenery::dataflow::light::Control::DISABLE:
+            spdlog::debug("Send manual disable");
+            request.set_control(ManualControlRequest::CONTROL_DISABLE);
+            break;
+        case open_greenery::dataflow::light::Control::TOGGLE:
+            spdlog::debug("Send manual toggle");
+            request.set_control(ManualControlRequest::CONTROL_TOGGLE);
+            break;
+        default:
+            assert(false && "Unknown light::Control type");
+    }
+
+    auto status = m_stub->ManualControl(&context, request, &response);
+    if (!status.ok())
+    {
+        spdlog::warn("Unsuccessful Relay::ManualControl request: {} {}",
+                     status.error_code(),
+                     status.error_message());
+    }
+}
+
+void Client::set(open_greenery::dataflow::light::Mode mode)
+{
+    grpc::ClientContext context;
+    ModeSetting request;
+    google::protobuf::Empty response;
+
+    switch (mode)
+    {
+        case open_greenery::dataflow::light::Mode::MANUAL:
+            spdlog::debug("Send mode setting: MANUAL");
+            request.set_mode(ModeSetting::MODE_MANUAL);
+            break;
+        case open_greenery::dataflow::light::Mode::AUTO:
+            spdlog::debug("Send mode setting: AUTO");
+            request.set_mode(ModeSetting::MODE_AUTO);
+            break;
+        default:
+            assert(false && "Unknown light::Mode type");
+    }
+
+    auto status = m_stub->SetMode(&context, request, &response);
+    if (!status.ok())
+    {
+        spdlog::warn("Unsuccessful Relay::SetMode request: {} {}",
+                     status.error_code(),
+                     status.error_message());
+    }
+}
+
+std::optional<bool> Client::get()
+{
+    grpc::ClientContext context;
+    google::protobuf::Empty request;
+    Status response;
+
+    auto status = m_stub->GetStatus(&context, request, &response);
+    if (!status.ok())
+    {
+        spdlog::warn("Unsuccessful Relay::GetStatus request: {} {}",
+                     status.error_code(),
+                     status.error_message());
+        return {};
+    }
+
+    return response.is_enabled();
+}
+
+}
---
+++ src/device/lib/rpc/relay/src/Server.cpp
@@ -0,0 +1,185 @@
+#include "open_greenery/rpc/relay/Server.hpp"
+#include <cassert>
+#include <utility>
+#include <grpcpp/server_builder.h>
+#include <spdlog/spdlog.h>
+#include <google/protobuf/empty.pb.h>
+
+
+namespace open_greenery::rpc::relay
+{
+
+grpc::Status Server::Service::SetConfig(
+        grpc::ServerContext * context,
+        const open_greenery::rpc::relay::Config * request,
+        google::protobuf::Empty * response)
+{
+    std::ignore = context;
+    std::ignore = response;
+    open_greenery::dataflow::light::LightConfigRecord received_config{
+            QTime::fromMSecsSinceStartOfDay(request->day_start()),
+            QTime::fromMSecsSinceStartOfDay(request->day_end())
+    };
+
+    auto TimeStr = [](const QTime & t) { return t.toString("hh:mm:ss").toStdString(); };
+    spdlog::debug("Relay config received: day start={}, end={}",
+                  TimeStr(received_config.day_start),
+                  TimeStr(received_config.day_end));
+
+    m_config_update_handler(received_config);
+    return {}; // OK
+}
+
+grpc::Status Server::Service::ManualControl(
+        grpc::ServerContext * context,
+        const open_greenery::rpc::relay::ManualControlRequest * request,
+        google::protobuf::Empty * response)
+{
+    std::ignore = context;
+    std::ignore = response;
+
+    open_greenery::dataflow::light::Control control;
+
+    switch (request->control())
+    {
+        case ManualControlRequest::CONTROL_ENABLE:
+            spdlog::debug("Manual enable received");
+            control = open_greenery::dataflow::light::Control::ENABLE;
+            break;
+        case ManualControlRequest::CONTROL_DISABLE:
+            spdlog::debug("Manual disable received");
+            control = open_greenery::dataflow::light::Control::DISABLE;
+            break;
+        case ManualControlRequest::CONTROL_TOGGLE:
+            spdlog::debug("Manual toggle received");
+            control = open_greenery::dataflow::light::Control::TOGGLE;
+            break;
+        default:
+            assert(false && "Unknown relay::Control type");
+    }
+    m_manual_control_handler(control);
+
+    return {};
+}
+
+grpc::Status Server::Service::SetMode(
+        grpc::ServerContext * context,
+        const open_greenery::rpc::relay::ModeSetting * request,
+        google::protobuf::Empty * response)
+{
+    std::ignore = context;
+    std::ignore = response;
+
+    open_greenery::dataflow::light::Mode mode;
+
+    switch (request->mode())
+    {
+        case ModeSetting::MODE_MANUAL:
+            spdlog::debug("Manual mode setting received");
+            mode = open_greenery::dataflow::light::Mode::MANUAL;
+            break;
+        case ModeSetting::MODE_AUTO:
+            spdlog::debug("Auto mode setting received");
+            mode = open_greenery::dataflow::light::Mode::AUTO;
+            break;
+        default:
+            assert(false && "relay relay::Mode type");
+    }
+
+    m_mode_update_handler(mode);
+
+    return {}; // OK
+}
+
+grpc::Status Server::Service::GetStatus(
+        grpc::ServerContext * context,
+        const google::protobuf::Empty * request,
+        open_greenery::rpc::relay::Status * response)
+{
+    std::ignore = context;
+    std::ignore = request;
+
+    response->set_is_enabled(m_status_request_handler());
+    spdlog::debug("{} status sent", (response->is_enabled() ? "Enabled" : "Disabled"));
+
+    return {}; // OK
+}
+
+Server::Server(const std::string & host)
+{
+    ::grpc::ServerBuilder builder;
+    builder.AddListeningPort(host, ::grpc::InsecureServerCredentials());
+    builder.RegisterService(&m_service);
+    m_server = builder.BuildAndStart();
+}
+
+Server::~Server()
+{
+    shutdown();
+}
+
+void Server::wait()
+{
+    assert(m_server && "LightProxyServer wait: server is nullptr");
+    m_server->Wait();
+}
+
+void Server::shutdown()
+{
+    assert(m_server && "LightProxyServer shutdown: server is nullptr");
+    m_server->Shutdown();
+}
+
+void Server::onConfigUpdate(
+        open_greenery::dataflow::common::AsyncReceive<
+                open_greenery::dataflow::light::LightConfigRecord> handler)
+{
+    m_service.m_config_update_handler = std::move(handler);
+}
+
+void Server::onManualControl(
+        open_greenery::dataflow::common::AsyncReceive
+                <open_greenery::dataflow::light::Control> handler)
+{
+    m_service.m_manual_control_handler = std::move(handler);
+}
+
+void Server::onModeUpdate(
+        open_greenery::dataflow::common::AsyncReceive
+                <open_greenery::dataflow::light::Mode> handler)
+{
+    m_service.m_mode_update_handler = std::move(handler);
+}
+
+void Server::onStatusRequest(open_greenery::dataflow::common::AsyncProvide<bool> handler)
+{
+    m_service.m_status_request_handler = std::move(handler);
+}
+
+void Server::onUpdate(
+        open_greenery::dataflow::common::AsyncReceive
+                <open_greenery::dataflow::light::LightConfigRecord> receive)
+{
+    onConfigUpdate(std::move(receive));
+}
+
+void Server::onUpdate(
+        open_greenery::dataflow::common::AsyncReceive
+                <open_greenery::dataflow::light::Control> receive)
+{
+    onManualControl(std::move(receive));
+}
+
+void Server::onUpdate(
+        open_greenery::dataflow::common::AsyncReceive
+                <open_greenery::dataflow::light::Mode> receive)
+{
+    onModeUpdate(std::move(receive));
+}
+
+void Server::onRequest(open_greenery::dataflow::common::AsyncProvide<bool> provide)
+{
+    onStatusRequest(std::move(provide));
+}
+
+}
