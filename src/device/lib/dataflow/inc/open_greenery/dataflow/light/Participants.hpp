7fc4eeeca237d6e0f25a9b106e8c4e5116f6329e
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-11-05 16:53:24 +0200

    Optional and non-optional light data providers

--- src/device/lib/dataflow/inc/open_greenery/dataflow/light/Participants.hpp
+++ src/device/lib/dataflow/inc/open_greenery/dataflow/light/Participants.hpp
@@ -14,28 +14,32 @@
 namespace open_greenery::dataflow::light
 {
 
-using IConfigProvider = open_greenery::dataflow::common::IOptionalProvider<LightConfigRecord>;
+using IConfigProvider = open_greenery::dataflow::common::IProvider<LightConfigRecord>;
+using IConfigOptionalProvider = open_greenery::dataflow::common::IOptionalProvider<LightConfigRecord>;
 using IConfigReceiver = open_greenery::dataflow::common::IReceiver<LightConfigRecord>;
 using IAsyncConfigProvider = open_greenery::dataflow::common::IAsyncProvider<LightConfigRecord>;
 using AsyncConfigProvider = open_greenery::dataflow::common::AsyncProvider<LightConfigRecord>;
 using IAsyncConfigRecevier = open_greenery::dataflow::common::IAsyncReceiver<LightConfigRecord>;
 using AsyncConfigRecevier = open_greenery::dataflow::common::AsyncReceiver<LightConfigRecord>;
 
-using IManualControlProvider = open_greenery::dataflow::common::IOptionalProvider<Control>;
+using IManualControlProvider = open_greenery::dataflow::common::IProvider<Control>;
+using IManualControlOptionalProvider = open_greenery::dataflow::common::IOptionalProvider<Control>;
 using IManualControlReceiver = open_greenery::dataflow::common::IReceiver<Control>;
 using IAsyncManualControlProvider = open_greenery::dataflow::common::IAsyncProvider<Control>;
 using AsyncManualControlProvider = open_greenery::dataflow::common::AsyncProvider<Control>;
 using IAsyncManualControlRecevier = open_greenery::dataflow::common::IAsyncReceiver<Control>;
 using AsyncManualControlRecevier = open_greenery::dataflow::common::AsyncReceiver<Control>;
 
-using IModeProvider = open_greenery::dataflow::common::IOptionalProvider<Mode>;
+using IModeProvider = open_greenery::dataflow::common::IProvider<Mode>;
+using IModeProviderOptionalProvider = open_greenery::dataflow::common::IOptionalProvider<Mode>;
 using IModeReceiver = open_greenery::dataflow::common::IReceiver<Mode>;
 using IAsyncModeProvider = open_greenery::dataflow::common::IAsyncProvider<Mode>;
 using AsyncModeProvider = open_greenery::dataflow::common::AsyncProvider<Mode>;
 using IAsyncModeRecevier = open_greenery::dataflow::common::IAsyncReceiver<Mode>;
 using AsyncModeRecevier = open_greenery::dataflow::common::AsyncReceiver<Mode>;
 
-using IStatusProvider = open_greenery::dataflow::common::IOptionalProvider<bool>;
+using IStatusProvider = open_greenery::dataflow::common::IProvider<bool>;
+using IStatusOptionalProvider = open_greenery::dataflow::common::IOptionalProvider<bool>;
 using IStatusReceiver = open_greenery::dataflow::common::IReceiver<bool>;
 using IAsyncStatusProvider = open_greenery::dataflow::common::IAsyncProvider<bool>;
 using AsyncStatusProvider = open_greenery::dataflow::common::AsyncProvider<bool>;
