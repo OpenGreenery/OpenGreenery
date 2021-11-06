fcb1143715c5fb94de00d03000500e1402416011
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-11-06 19:09:41 +0200

    Relay RPC tests actualization

--- src/device/tests/lib/rpc/relay/RelayRPCTest.cpp
+++ src/device/tests/lib/rpc/relay/RelayRPCTest.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 #include <memory>
 
-#include <open_greenery/rpc/light/LightProxyClient.hpp>
-#include <open_greenery/rpc/light/LightProxyServer.hpp>
+#include <open_greenery/rpc/relay/Client.hpp>
+#include <open_greenery/rpc/relay/Server.hpp>
 
-namespace ogrpcl = open_greenery::rpc::light;
+namespace ogrpcr = open_greenery::rpc::relay;
 namespace ogdfl = open_greenery::dataflow::light;
 
 namespace open_greenery::tests::lib::rpc
@@ -17,75 +17,54 @@ protected:
     void SetUp() override
     {
         constexpr static char HOST [] {"127.0.0.1:8081"};
-        client = std::make_shared<ogrpcl::LightProxyClient>(HOST);
-        server = std::make_shared<ogrpcl::LightProxyServer>(HOST);
-        control_provider = client->getManualControlProvider();
-        control_receiver = server;
-        config_provider = client->getConfigProvider();
-        config_receiver = server;
-        mode_provider = client->getModeProvider();
-        mode_receiver = server;
-        status_provider = server;
-        status_receiver = client->getStatusReceiver();
+        client = std::make_shared<ogrpcr::Client>(HOST);
+        server = std::make_shared<ogrpcr::Server>(HOST);
+        control_provider = server;
+        control_receiver = client;
+        config_provider = server;
+        config_receiver = client;
+        mode_provider = server;
+        mode_receiver = client;
+        status_provider = client;
+        status_receiver = server;
     }
 
-    std::shared_ptr<ogrpcl::LightProxyClient> client;
-    std::shared_ptr<ogrpcl::LightProxyServer> server;
+    std::shared_ptr<ogrpcr::Client> client;
+    std::shared_ptr<ogrpcr::Server> server;
 
-    std::shared_ptr<ogdfl::IConfigProvider> config_provider;
+    std::shared_ptr<ogdfl::IAsyncConfigProvider> config_provider;
     std::shared_ptr<ogdfl::IConfigReceiver> config_receiver;
-    std::shared_ptr<ogdfl::IManualControlProvider> control_provider;
+    std::shared_ptr<ogdfl::IAsyncManualControlProvider> control_provider;
     std::shared_ptr<ogdfl::IManualControlReceiver> control_receiver;
-    std::shared_ptr<ogdfl::IModeProvider> mode_provider;
+    std::shared_ptr<ogdfl::IAsyncModeProvider> mode_provider;
     std::shared_ptr<ogdfl::IModeReceiver> mode_receiver;
-    std::shared_ptr<ogdfl::IStatusProvider> status_provider;
-    std::shared_ptr<ogdfl::IStatusReceiver> status_receiver;
+    std::shared_ptr<ogdfl::IStatusOptionalProvider> status_provider;
+    std::shared_ptr<ogdfl::IAsyncStatusRecevier> status_receiver;
 };
 
 TEST_F(RelayRPCTest, ConfigTransmition)
 {
     const ogdfl::LightConfigRecord expected_config {QTime(0, 0),
                                                     QTime(23, 59, 59, 999)};
+
+    config_provider->onUpdate([expected_config](ogdfl::LightConfigRecord actual_config){
+        EXPECT_EQ(expected_config.day_start, actual_config.day_start);
+        EXPECT_EQ(expected_config.day_end, actual_config.day_end);
+    });
     config_receiver->set(expected_config);
-    const auto actual_config = config_provider->get();
-    ASSERT_TRUE(actual_config.has_value());
-    EXPECT_EQ(expected_config.day_start, actual_config->day_start);
-    EXPECT_EQ(expected_config.day_end, actual_config->day_end);
-}
-
-TEST_F(RelayRPCTest, SingleConfigTransmition)
-{
-    EXPECT_FALSE(config_provider->get().has_value());
-
-    config_receiver->set({QTime(), QTime()});
-    EXPECT_TRUE(config_provider->get().has_value());
-
-    // Second read shall return empty config
-    EXPECT_FALSE(config_provider->get().has_value());
 }
 
 TEST_F(RelayRPCTest, ManualControlTransmition)
 {
-    for (auto expected_config : {ogdfl::Control::ENABLE,
+    for (auto expected_ctl : {ogdfl::Control::ENABLE,
                                  ogdfl::Control::DISABLE,
                                  ogdfl::Control::TOGGLE})
     {
-        control_receiver->set(expected_config);
-        auto actual_config = control_provider->get();
-        ASSERT_TRUE(actual_config.has_value());
-        EXPECT_EQ(expected_config, actual_config.value());
+        control_provider->onUpdate([expected_ctl](ogdfl::Control actual_ctl){
+            EXPECT_EQ(expected_ctl, actual_ctl);
+        });
+        control_receiver->set(expected_ctl);
     }
-}
-
-TEST_F(RelayRPCTest, SingleManualControlTransmition)
-{
-    EXPECT_FALSE(control_provider->get().has_value());
-
-    control_receiver->set(ogdfl::Control::ENABLE);
-    EXPECT_TRUE(control_provider->get().has_value());
-
-    // Second read shall return empty control
-    EXPECT_FALSE(control_provider->get().has_value());
 }
 
 TEST_F(RelayRPCTest, ModeTransmition)
@@ -93,44 +72,24 @@ TEST_F(RelayRPCTest, ModeTransmition)
     for (auto expected_mode : {ogdfl::Mode::AUTO,
                                ogdfl::Mode::MANUAL})
     {
+        mode_provider->onUpdate([expected_mode](ogdfl::Mode actual_mode){
+            EXPECT_EQ(expected_mode, actual_mode);
+        });
         mode_receiver->set(expected_mode);
-        auto actual_mode = mode_provider->get();
-        ASSERT_TRUE(actual_mode.has_value());
-        EXPECT_EQ(expected_mode, actual_mode.value());
     }
-}
-
-TEST_F(RelayRPCTest, SingleModeTransmition)
-{
-    EXPECT_FALSE(mode_provider->get().has_value());
-
-    mode_receiver->set(ogdfl::Mode::AUTO);
-    EXPECT_TRUE(mode_provider->get().has_value());
-
-    // Second read shall return empty mode
-    EXPECT_FALSE(mode_provider->get().has_value());
 }
 
 TEST_F(RelayRPCTest, StatusTransmition)
 {
     for (auto expected_status : {true, false})
     {
-        status_receiver->set(expected_status);
-        auto actual_status = status_provider->get();
+        status_receiver->onRequest([expected_status]{
+            return expected_status;
+        });
+        const auto actual_status = status_provider->get();
         ASSERT_TRUE(actual_status.has_value());
-        EXPECT_EQ(expected_status, actual_status.value());
+        EXPECT_EQ(expected_status, *actual_status);
     }
-}
-
-TEST_F(RelayRPCTest, SingleStatusTransmition)
-{
-    EXPECT_FALSE(status_provider->get().has_value());
-
-    status_receiver->set(true);
-    EXPECT_TRUE(status_provider->get().has_value());
-
-    // Second read shall return empty mode
-    EXPECT_FALSE(status_provider->get().has_value());
 }
 
 }
