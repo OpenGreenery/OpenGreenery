dadd349f9dc6b8ab424506bea6e35155f0da74e9
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2021-11-16 23:55:11 +0200

    Android app: Service status request on activity create

---
+++ src/android/app/src/main/java/com/open_greenery/mobile/Config.kt
@@ -0,0 +1,5 @@
+package com.open_greenery.mobile
+
+import java.time.LocalTime
+
+data class Config(val dayStart: LocalTime, val dayEnd: LocalTime)
---
+++ src/android/app/src/main/java/com/open_greenery/mobile/ServiceStatus.kt
@@ -0,0 +1,5 @@
+package com.open_greenery.mobile
+
+data class ServiceStatus(val mode: Mode,
+                         val relayEnabled: Boolean,
+                         val config: Config)
